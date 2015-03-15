c7371f8fcefe2de8ecf527523ac75f22e8e113ed
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-03-15 21:01:40 +0100

    Add script for forcing resolution.

---
+++ bin/force_resolution.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --newmode "2560x1440_30.00"  146.25  2560 2680 2944 3328  1440 1443 1448 1468 -hsync +vsync
+xrandr --addmode HDMI1 2560x1440_30.00
+xrandr --output HDMI1 --mode 2560x1440_30.00
+
+nitrogen --restore
