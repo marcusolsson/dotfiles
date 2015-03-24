72e2dc2182e2f72edf8406ab948051c9d75ed00b
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-03-24 22:07:25 +0100

    Add font-resize extension to URxvt

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@
 
 DOTFILES=$HOME/dotfiles
 
+# files
 ln -sf $DOTFILES/xprofile $HOME/.xprofile
 ln -sf $DOTFILES/Xdefaults $HOME/.Xdefaults
 ln -sf $DOTFILES/vimrc $HOME/.vimrc
-ln -sf $DOTFILES/i3 $HOME/.i3
 ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
+
+# folders
+ln -sf $DOTFILES/i3 $HOME/.i3
+ln -sf $DOTFILES/urxvt $HOME/.urxvt
