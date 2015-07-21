e69536838606eef14af989f2deefd9251038221f
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-07-21 16:16:27 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh 
+
+apt-get install i3 volumeicon-alsa nitrogen python3-udiskie
+apt-get install vim git
+apt-get install gnome-terminal ranger gpicview
+apt-get install lxappearance gtk-chtheme qt4-qtconfig
