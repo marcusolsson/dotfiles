5312d6fe3cdc4151834fe1e9222107a976f0280e
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-07-22 09:12:43 +0200

    Add basrc files to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 ln -sf $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/xsessionrc $HOME/.xsessionrc
+ln -sf $HOME/dotfiles/bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/bash_profile $HOME/.bash_profile
 
 mkdir -p $HOME/.i3
 ln -sf $HOME/dotfiles/i3/config $HOME/.i3/config
