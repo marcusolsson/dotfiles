1bf0d3d013ea75457be2e56646a70a93ebe34e1c
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-07-28 10:44:15 +0200

    Revert to urxvt after fixing font issues

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/xsessionrc $HOME/.xsessionrc
 ln -sf $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/dotfiles/bash_profile $HOME/.bash_profile
+ln -sf $HOME/dotfiles/Xdefaults $HOME/.Xdefaults
 
 mkdir -p $HOME/.i3
 ln -sf $HOME/dotfiles/i3/config $HOME/.i3/config
