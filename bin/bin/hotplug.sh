1bd78fdaf8a9264eb5a61d236225300405ed7fe3
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-12-05 12:41:14 +0100

    Add hotplug script

---
+++ bin/bin/hotplug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/marcus/.Xauthority
+
+DP1=`xrandr | grep "^DP1 connected"`
+DP2=`xrandr | grep "^DP2 connected"`
+
+if [ -n "$DP1" ]; then
+	xrandr --output DP1 --preferred --primary --output eDP1 --off
+elif [ -n "$DP2" ]; then
+	xrandr --output DP2 --preferred --primary --output eDP1 --off
+else
+	xrandr --output eDP1 --preferred --primary
+fi
+
+nitrogen --restore
