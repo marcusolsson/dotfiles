85cd03d4df90051fd2405f488dfa09cdbb21f372
Marcus Olsson <olsson.e.marcus@gmail.com>
2019-05-27 13:49:30 +0200

    Add tpm and vim-plug to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,10 @@
 for dir in $( ls -A -d */ | grep -vE '\.git$' ) ; do
     echo stow $dir -t $HOME
 done
+
+# Install Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
